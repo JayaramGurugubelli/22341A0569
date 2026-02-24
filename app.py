2e9ea9595808f3d13171e2a070a5c0ecbfd14bfb
Jayaram <jayaramgurugubelli4@gmail.com>
2026-02-24 10:59:12 +0530

    first commit

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return """
-    <h1>🚀 Hello Rajesh!</h1>
-    <h2>This App is Running from GitHub Actions Pipeline</h2>
+    <h1>🚀 Hello Jayaram!</h1>
+    <p>🎉 Welcome to the CI/CD Demo App!</p>
+    <p>👨‍💻 This app is designed to demonstrate the power of Continuous Integration and Continuous Deployment.</p>
+    <p>🔧 With CI/CD, we can automate the testing and deployment of our applications, making it easier to deliver high-quality software faster.</p>
+    <p>🌟 Enjoy exploring the world of CI/CD and happy coding!</p>
     """
 
 if __name__ == "__main__":
